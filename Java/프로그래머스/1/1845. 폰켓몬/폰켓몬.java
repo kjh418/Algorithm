0c4da9e36ca58db4d31717d177e71f0129adc300
kjh418 <159685512+kjh418@users.noreply.github.com>
2024-07-27 21:14:26 +0900

    [level 1] Title: 폰켓몬, Time: 7.18 ms, Memory: 86.6 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-    	// 뽑을 수 있는 폰켓몬 수는 N/2
-        int answer = nums.length/2;
+        int answer = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
         
-        // 중복된 수는 제외하기 위해 사용(온전히 뽑을 폰켓몬 번호)
-        Set<Integer> set = new HashSet<>();
-        
-        // 뽑을 수 있는 폰켓몬 수에 맞춰 폰켓몬 번호 세팅
-        for(int num : nums){
-            set.add(num);
+        for (int num : nums) {
+        	// map에 폰켓몬 번호와 번호의 개수를 저장한다.
+            // num에 이미 번호가 존재하면 개수에 +1을 해주고 없으면 기본값 0을 세팅한다.
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        // 뽑을 수 있는 폰켓몬 수와 비교하여 작은 값 반환
-        // 최대한으로 뽑을 수 있는 폰켓몬 수가 answer기 때문에 그 값을 넘어가지 않게 하기 위함.
-        return set.size() > answer ? answer : set.size();
+
+        // 서로 다른 포켓몬 종류의 수
+        int diffPokemonCount = map.size();
+
+        // diffPokemonCount와 answer 중 더 작은 값 반환.
+        // 선택할 수 있는 폰켓몬 수를 최대화 하기 위함.
+        return Math.min(diffPokemonCount, answer);
     }
 }
