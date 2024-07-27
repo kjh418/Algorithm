cce6567fc648a93e41c58889a2ef9364bc1d8fc9
kjh418 <159685512+kjh418@users.noreply.github.com>
2024-07-27 21:03:24 +0900

    [level 1] Title: 폰켓몬, Time: 3.58 ms, Memory: 82.1 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,26 +1,20 @@
 import java.util.*;
 
-/*
-    4마리라면 2마리를 가지고 갈 수 있다. 
-    그러나 폰켓몬의 종류는 3종류,
-    그렇게 되면 n/2값을 반환해야 한다. 
-    
-    6마리라면 3마리를 가지고 갈 수 있다.
-    폰켓몬 종류가 2종류라면,
-    종류를 반환해주면 된다.
-*/
-
 class Solution {
     public int solution(int[] nums) {
+    	// 뽑을 수 있는 폰켓몬 수는 N/2
         int answer = nums.length/2;
         
-        // 중복된 수는 제외하기 위해 사용
+        // 중복된 수는 제외하기 위해 사용(온전히 뽑을 폰켓몬 번호)
         Set<Integer> set = new HashSet<>();
         
+        // 뽑을 수 있는 폰켓몬 수에 맞춰 폰켓몬 번호 세팅
         for(int num : nums){
             set.add(num);
         }
         
+        // 뽑을 수 있는 폰켓몬 수와 비교하여 작은 값 반환
+        // 최대한으로 뽑을 수 있는 폰켓몬 수가 answer기 때문에 그 값을 넘어가지 않게 하기 위함.
         return set.size() > answer ? answer : set.size();
     }
 }
